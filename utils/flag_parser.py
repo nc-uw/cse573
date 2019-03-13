9729ebb566fad96173388114fc33b51096e2bdf6
Namit Chauhan <nc57@uw.edu>
2019-03-13 15:01:22 -0700

    update args:action-space

--- utils/flag_parser.py
+++ utils/flag_parser.py
@@ -34,7 +34,8 @@ def parse_arguments():
     parser.add_argument(
         '--action-space',
         type=int,
-        default=6,
+        delta=2
+        default=6 +delta,
         metavar='AS',
         help='# of actions (default: 5)')
     parser.add_argument(
